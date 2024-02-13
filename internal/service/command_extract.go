386f3907b3cfc3934f322889d3823dd07ca65dcd
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2024-02-13 18:02:27 +0700

    Add comment

--- internal/service/command_extract.go
+++ internal/service/command_extract.go
@@ -7,11 +7,15 @@ import (
 )
 
 func ExtractCommand(message string) model.Command {
+	// Extract command and argument
 	commandAndArgs := strings.Split(message, ":")
 
 	args := []string{}
+
+	// Check if it has arguments
 	if len(commandAndArgs) > 1 {
 		tmp := strings.Split(commandAndArgs[1], ",")
+		// Remove whitespace and begin and end of each argument
 		for index, arg := range tmp {
 			tmp[index] = strings.Trim(arg, " ")
 		}
